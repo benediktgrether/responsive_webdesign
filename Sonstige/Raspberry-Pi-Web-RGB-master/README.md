4501f72643c812d2ffbb6d73cdeb546638672b66
Benedikt Grether <info@benediktgrether.de>
2019-04-30 11:44:55 +0200

    Add Web Interface for LED's

---
+++ Sonstige/Raspberry-Pi-Web-RGB-master/README.md
@@ -0,0 +1,24 @@
+# Simple RGB LED Controller
+
+This example is a simple GPIO LED controller for the Raspberry Pi. It lets you turn a red, blue and green LED on and off through a Javascript/jQuery powered Web-interface.
+
+## Requirements
+
+You'll need Python Flask installed to run this example, install with:
+
+```bash
+sudo apt-get install python-pip
+pip install flask
+```
+
+## Running
+
+Start up with:
+
+```bash
+sudo python ./rgb.py
+```
+
+And then visit:
+
+http://[your Pi address>]:5000/ui.html
---
+++ Sonstige/Raspberry-Pi-Web-RGB-master/rgb.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from flask import Flask
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+app = Flask(__name__)
+
+# Explorer HAT uses GPIO 27 = red, 4 = blue, 5 = green
+# Pibrella uses GPIO 27 = red, 17 = yellow, 4 = green
+
+
+# Map of LED names and associated GPIO pins
+leds = {
+    'red':    23,
+    'blue':   4,
+    'green':  24
+}
+
+for color in leds.keys():
+    GPIO.setup(leds[color], GPIO.OUT)
+
+@app.route("/led/<color>/<state>")
+def set_led(color, state):
+    if color in leds.keys():
+      if state == 'on':
+          GPIO.output(leds[color], 1)
+          return 'LED On: {}'.format(color)
+      else:
+          GPIO.output(leds[color], 0)
+          return 'LED Off: {}'.format(color)
+
+    return 'Invalid LED: {}'.format(color)
+
+app.add_url_rule("/", "index", lambda: 'Hello World!')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
